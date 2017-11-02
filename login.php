3be8cf2f7684e3c30d456f860b1faaf79e6af40a
lucywalsh <lucyhannahwalsh@gmail.com>
2017-11-02 23:34:26 +0000

    added database connection and insert into userLogin table

--- login.php
+++ login.php
@@ -1,9 +1,33 @@
 <!--PHP-->
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:team5logbook.database.windows.net,1433; Database = logbookdatabase", "team5logbook", "adminPassword123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "team5logbook@team5logbook", "pwd" => "adminPassword123", "Database" => "logbookdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:team5logbook.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
 <html>
 <body>
+<?php
+	$name = echo $_GET["name"];
+	$email = echo $_GET["email"];
+?>
+	Welcome <?php $name;?><br>
+	Your email address is: <?php $email; ?><br>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+<?php
+	$sql = "INSERT INTO userLogin (name, email) VALUES $name, $email";
+	?>
 
 </body>
 </html>
