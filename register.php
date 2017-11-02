f3cf76e6e8988ef96ab2b2e1f297342c43972324
lucywalsh <lucyhannahwalsh@gmail.com>
2017-11-02 23:41:54 +0000

    switched login php stuff to correct place in register

---
+++ register.php
@@ -0,0 +1,31 @@
+<!--PHP-->
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:team5logbook.database.windows.net,1433; Database = logbookdatabase", "team5logbook", "adminPassword123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "team5logbook@team5logbook", "pwd" => "adminPassword123", "Database" => "logbookdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:team5logbook.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
+<html>
+<body>
+<?php
+	$name = echo $_GET["name"];
+	$email = echo $_GET["email"];
+?>
+	Welcome <?php $name;?><br>
+	Your email address is: <?php $email; ?><br>
+
+
+
+</body>
+</html>
