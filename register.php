497938224cfe861cd3d3eee396abeef8b938cf33
lucywalsh <lucyhannahwalsh@gmail.com>
2017-11-03 11:38:36 +0000

    changed database connection and query

--- register.php
+++ register.php
@@ -1,30 +1,34 @@
 <!--PHP-->
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:team5logbook.database.windows.net,1433; Database = logbookdatabase", "team5logbook", "adminPassword123");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    $myServer = "team5logbook.database.windows.net";
+    $myUser = "team5logbook";
+    $myPass = "adminPassword123";
+    $myDB = "logbookdatabase";
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "team5logbook@team5logbook", "pwd" => "adminPassword123", "Database" => "logbookdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:team5logbook.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+    //connection to the database
+    $dbhandle = mssql_connect($myServer, $myUser, $myPass)
+      or die("Couldn't connect to SQL Server on $myServer");
+
+    //select a database to work with
+    $selected = mssql_select_db($myDB, $dbhandle)
+      or die("Couldn't open database $myDB");
+
+    //declare the SQL statement that will query the database
+    $query = "INSERT INTO userLogin (name, email) VALUES ('Jess','Jess@gmail.com')";
+
+
+    //execute the SQL query and return records
+    $result = mssql_query($query);
+
+    //close the connection
+    mssql_close($dbhandle);
+
 ?>
 
 <html>
 <body>
 	Welcome <?php echo $_GET["name"];?><br>
 	Your email address is: <?php echo $_GET["email"]; ?><br>
-
-	<?php
-		$query = "INSERT INTO userLogin (name, email) VALUES ('Jess','Jess@gmail.com')";
-		$result = mssql_query($query);
-	?>
 
 </body>
 </html>
