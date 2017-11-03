5aebda9e5531f6d6ac7eade52bd49714b308f336
lucywalsh <lucyhannahwalsh@gmail.com>
2017-11-03 12:15:53 +0000

    removed 1 echo statement

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 	Welcome <?php echo $_GET["name"];?><br>
-	Your email address is: <?php echo $_GET["email"];echo "hello"; ?><br>
+	Your email address is: <?php echo $_GET["email"]; ?><br>
 
 <!--PHP-->
 <?php
